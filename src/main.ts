cec0a223a7b9c2554568f95466e9add00d53bffd
Levan Ilashvili <levan@ilashvili.me>
2023-04-04 05:00:29 +0400

    Added helmet, enabled CORS

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Weather API')
     .setDescription('API for Agora :)')
